62a787ea7de5b6360d5026bcdb6f7e861ab8ec0f
snidhedinesh <snidhe2016@gmail.com>
2025-06-12 19:00:25 +0530

    new-commit

---
+++ home.php
@@ -0,0 +1,311 @@
+<?php include 'includes/header.php' ?>
+
+  <!-- Hero Section -->
+  <div class="hero">
+
+    <div class="home-img-new bg-white">
+
+      <div class="overlay-div">
+        <div class="div-text">
+          <p>VISHAL TRAVELS</p>
+          <h1>Trusted by thousands of people<br>all across the country</h1>
+          <a class="button">Book Now</a>
+
+        </div>
+        <div class="div-img align">
+          <img class="blue-bus" src="bootstrap/images/blue-bus.jpg">
+          <img class="orange-bus" src="bootstrap/images/orange-bus2.png">
+
+        </div>
+      </div>
+
+      <!-- Booking / Call Box overlay -->
+      <div class="booking-wrapper">
+        <div class="booking">
+          
+          <div class="form-box">
+            <form>
+               <div> <label for="">From city</label>
+              <select>
+                <option>select from city</option>
+              </select></div>
+              <div> <label for="">From city</label>
+              <select>
+                <option>select from city</option>
+              </select></div>
+             
+             <div>
+                <label  for="">journey Date</label>
+              <input type="date" placeholder="dd‑mm‑yyyy" />
+
+             </div>
+             <div>
+                <label  for="">Return Date</label>
+              <input type="date" placeholder="dd‑mm‑yyyy" />
+
+             </div>
+              
+              <button type="submit"><i class="bi bi-search"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="marquee-container margin-top">
+  <div class="marquee">
+  <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+
+  <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+
+  <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+
+  <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+
+  <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+
+    <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+    <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+    <span>🧭 Traveling To Different Destinations And Exploring Unknown Places Is The Most Fantastic Feeling</span>
+  </div>
+</div>
+  <section class="container-div ">
+  <section>
+
+
+</section>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div>
+          <img class="rental-img" src="bootstrap/images/travel-img.jpg" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12 image-top">
+        <h1>A bus rental that's affordable and comfortable</h1>
+        <p>Traveling to different destinations and exploring unknown places is the most fantastic feeling. People have
+          recently started traveling a lot more than they used to. Moreover, there has been a surge in tourists who are
+          taking road trips. One can simply make an online cab booking and travel to any part of the country</p>
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Wide Range of Destinations.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Expertise and Experience.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Exceptional Customer Service.</p>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Wide Range of Destinations.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Expertise and Experience.</p>
+            </div>
+            <div class="align-item"><img class="me-2" src="bootstrap/images/tick .png" alt="">
+              <p class="m-0">Exceptional Customer Service.</p>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+  </section>
+
+  <section class="container-div">
+    <section class="pricing-section">
+      <h1>Our bus rental price/rate per km</h1>
+      <p>Get the best rates for all your vehicle rental needs from Vishal travels</p>
+
+      <div class="pricing-cards">
+        <div class="card-price text-left">
+        
+
+          <img src="bootstrap/images/bus-rental-1.png" alt="Bus Image">
+
+          
+         
+            <h4 class="features-h4">Tourer 400</h4>
+           
+      
+
+       
+          <label class="features-label">Executive</label>
+          <hr class="my-2">
+          <div class="div-inline">
+            <div class="price">₹2,500/day</div>
+            <button class="price-btn">Book Now</button>
+          </div>
+        </div>
+        <div class="card-price text-left">
+        
+
+        <img src="bootstrap/images/bus-rental-2.png" alt="Bus Image">
+
+        
+       
+          <h4 class="features-h4">Tourer 400</h4>
+         
+    
+
+     
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+      <div class="card-price text-left">
+        
+
+        <img src="bootstrap/images/bus-rental-3.png" alt="Bus Image">
+
+        
+       
+          <h4 class="features-h4">Tourer 400</h4>
+         
+    
+
+     
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+
+        <div class="card-price text-left">
+        
+
+        <img src="bootstrap/images/bus-rental-4.png" alt="Bus Image">
+
+        
+       
+          <h4 class="features-h4">Tourer 400</h4>
+         
+    
+
+     
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+      </div>
+    </section>
+  </section>
+
+  <!-- FAQ + Contact Section -->
+  <section class="faq-contact-gray">
+    <section class="container-div">
+
+      <div class="content">
+        <!-- FAQ -->
+        <div class="faq">
+          <h3>Frequently Asked Question</h3>
+          <div class="question">
+            <h4>What transportation services does Vishal travels offer?</h4>
+            <p>Vishal Travels offers Bus rentals, airport transfers, chauffeur-driven services, and outstation services.
+            </p>
+          </div>
+          <div class="question">
+            <strong>Do you offer transportation services for events and weddings?</strong><br>
+            Yes, we provide vehicle rental services for special events and weddings.
+          </div>
+          <div class="question">
+            <strong>Do you offer transportation services in multiple cities?</strong><br>
+            Yes, our services are available across various cities.
+          </div>
+        </div>
+
+        <!-- Contact Form -->
+        <div class="contact-form">
+          <h3>Get In Touch</h3>
+          <input type="text" placeholder="Enter Full Name">
+          <input type="email" placeholder="Enter Your Email">
+          <input type="text" placeholder="Enter Phone Number">
+          <textarea rows="4" placeholder="Enter your Message"></textarea>
+          <button>Submit now</button>
+        </div>
+      </div>
+    </section>
+  </section>
+  <section class="facilities-section">
+    <div class="facilities-left">
+      <img src="bootstrap/images/bus-img-2.png" alt="Bus Image">
+    </div>
+    <div class="facilities-right">
+      <h2>Facilities we provide</h2>
+      <p>Get the best from Vishaltravels</p>
+      <div class="facility-boxes">
+        <div class="facility-box">
+          <span>01</span>
+          <h4>Pune Local Packages, Corporate Events, Marriages Events Services.</h4>
+          <p>Pune local & reliable and efficient transportation system. Corporate sector thrives in Pune’s diverse economy, and the city offers a vibrant array of Events catering.</p>
+        </div>
+        <div class="facility-box">
+          <span>02</span>
+          <h4>Pune to Outstation Bus & Tempo Traveller Services All Packages.</h4>
+          <p>From the picturesque hill stations of Maharashtra and Uttarakhand to the serene beaches of Goa and Konkan, we take you on a captivating voyage.</p>
+        </div>
+        <div class="facility-box">
+          <span>03</span>
+          <h4>Pune to Shirdi, Nashik Bus & Tempo Traveller Services.</h4>
+          <p>Pune to Shirdi Bus services provide convenient and comfortable transportation for pilgrims and travelers seeking a spiritual journey.</p>
+        </div>
+        <div class="facility-box">
+          <span>04</span>
+          <h4>Customizable Packages</h4>
+          <p>Why not offer customizable packages, allowing you to tailor your journey according to your preferences and schedule.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container-div">
+    <div class="testimonial-section">
+      <h1>Customer testimonial</h1> 
+      <p>Get the best  from Vishal travels </p>
+  
+      <div class="testimonial-container">
+        <div class="testimonial-side">
+          <h3>From the first stop to the final destination, Vishal travel bus journey was more than a ride</h3>
+          <img src="bootstrap/images/customer.jpg" alt="Customer" />
+        </div>
+  
+        <div class="testimonial-cards">
+          <div class="testimonial-slider" id="testimonialSlider">
+            <div class="testimonial-card">
+              <h4>Nidhi Verma</h4>
+              <p>I had the pleasure of booking trips with Vishal Travels, and it was an incredible experience. As a first-time solo traveler, I had my concerns, but the team at Vishal Travels put me at ease with their warmth and professionalism.</p>
+            </div>
+            <div class="testimonial-card">
+              <h4>Nidhi Verma</h4>
+              <p>I had the pleasure of booking trips with Vishal Travels, and it was an incredible experience. As a first-time solo traveler, I had my concerns, but the team at Vishal Travels put me at ease with their warmth and professionalism.</p>
+            </div>
+            <div class="testimonial-card">
+              <h4>Nidhi Verma</h4>
+              <p>I had the pleasure of booking trips with Vishal Travels, and it was an incredible experience. As a first-time solo traveler, I had my concerns, but the team at Vishal Travels put me at ease with their warmth and professionalism.</p>
+            </div>
+          </div>
+          <div class="slider-controls">
+            <button onclick="moveSlide(-1)">‹</button>
+            <button onclick="moveSlide(1)">›</button>
+          </div>
+        </div>
+      </div>
+  
+     
+    </div>
+  </section>
+
+  <?php include 'includes/footer.php' ?>
+
+ 
