e250e8c2d762ed9c670dadc3d76cea968923b04f
snidhedinesh <snidhe2016@gmail.com>
2025-06-13 19:02:25 +0530

    new-commit

---
+++ bus-packages.php
@@ -0,0 +1,244 @@
+<?php include 'includes/header.php' ?>
+
+<!-- Hero Section -->
+<div class="top-div">
+
+  <div class="home-img-new bg-white">
+
+    <div class="overlay-div">
+      <div class="div-text">
+
+        <h1>Bus Package </h1>
+        <p>home>>package</p>
+
+      </div>
+
+    </div>
+
+  </div>
+</div>
+</section>
+
+
+<section class="container-div">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-1.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-2.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-3.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-4.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-1.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-2.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-3.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-4.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+ 
+   
+  </viv>
+</section>
+<section class="contact-banner">
+    <div class="content">
+      <h2>+91 9876543XXX</h2>
+      <h1>CONTACT US AND BOOK YOUR DREAM JOURNEY</h1>
+      <button class="btn">CONTACT NOW</button>
+    </div>
+  </section>
+
+  <section class="cards-section">
+    <!-- Card 1 -->
+    <div class="location-card">
+      <img src="bootstrap/images/location-img.png" alt="Mumbai" />
+      <div class="location-card-content">
+        <div class="d-align">
+        <h3>Pune to Mumbai</h3><div class="stars">★★★★★</div></div>
+        <p>Duration: 2 days / 1 night</p>
+        <p>Package includes: Sightseeing | Tour Guide | Hotel</p>
+        
+        <a href="#" class="book-btn">Book Now</a>
+      </div>
+    </div>
+    <div class="location-card">
+      <img src="bootstrap/images/location-img.png" alt="Mumbai" />
+      <div class="location-card-content">
+        <div class="d-align">
+        <h3>Pune to Mumbai</h3><div class="stars">★★★★★</div></div>
+        <p>Duration: 2 days / 1 night</p>
+        <p>Package includes: Sightseeing | Tour Guide | Hotel</p>
+        
+        <a href="#" class="book-btn">Book Now</a>
+      </div>
+    </div>
+    <div class="location-card">
+      <img src="bootstrap/images/location-img.png" alt="Mumbai" />
+      <div class="location-card-content">
+        <div class="d-align">
+        <h3>Pune to Mumbai</h3><div class="stars">★★★★★</div></div>
+        <p>Duration: 2 days / 1 night</p>
+        <p>Package includes: Sightseeing | Tour Guide | Hotel</p>
+        
+        <a href="#" class="book-btn">Book Now</a>
+      </div>
+    </div>
+  </section>
+
+
+<?php include 'includes/footer.php' ?>
---
+++ package.php
@@ -0,0 +1,201 @@
+<?php include 'includes/header.php' ?>
+
+<!-- Hero Section -->
+<div class="top-div">
+
+  <div class="home-img-new bg-white">
+
+    <div class="overlay-div">
+      <div class="div-text">
+
+        <h1>Our Package </h1>
+        <p>home>>package</p>
+
+      </div>
+
+    </div>
+
+  </div>
+</div>
+</section>
+
+
+<section class="container-div">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-1.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-2.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-3.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-4.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-1.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-2.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-3.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <div class="card-price text-left">
+
+
+        <img src="bootstrap/images/bus-rental-4.png" alt="Bus Image">
+
+
+
+        <h4 class="features-h4">Tourer 400</h4>
+
+
+
+
+        <label class="features-label">Executive</label>
+        <hr class="my-2">
+        <div class="div-inline">
+          <div class="price">₹2,500/day</div>
+          <button class="price-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+ 
+   
+  </viv>
+</section>
+
+
+
+<?php include 'includes/footer.php' ?>
